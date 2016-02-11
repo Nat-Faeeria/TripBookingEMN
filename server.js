222f7cd92cec16fde8f5dc4fa7e1184d389599d6
Faeeria <nathanael.gimenez@serli.com>
2016-02-11 17:12:24 +0100

    fixed server.js

--- server.js
+++ server.js
@@ -1,18 +1,11 @@
 var fs = require('fs');
 var path = require('path');
 var express = require('express');
-var bp = require('body-parser');
 var app = express();
 
 app.set('port', (process.env.PORT || 3000));
 
 app.use('/', express.static(path.join(__dirname, 'public')));
-
-
-app.post('/com', function(req,rep){
-  console.log(req.body);
-  console.log("AAAAAAH");
-});
 
 app.listen(app.get('port'), function() {
   console.log('Server started: http://localhost:' + app.get('port') + '/');
